0061e40ef6d581dde835ad02c06a3af8fc5c1423
psyriccio <psyriccio@psyzzy.net>
2015-05-27 17:09:39 +0300

    mediaType support

--- src/mokko/uds/ServerResponce.java
+++ src/mokko/uds/ServerResponce.java
@@ -14,17 +14,26 @@ import java.io.UnsupportedEncodingException;
  */
 public class ServerResponce implements Serializable {
 
+    private String mediaType;
     private byte[] data;
 
     public ServerResponce() {
         data = null;
+        mediaType = "";
     }
 
     public ServerResponce(byte[] data) {
+        mediaType = "text/plain";
         this.data = data;
     }
 
+    public ServerResponce(String mediaType, byte[] data) {
+        this.mediaType = mediaType;
+        this.data = data;
+    }
+    
     public ServerResponce(String data) {
+        mediaType = "text/plain";
         try {
             this.data = data.getBytes("UTF-8");
         } catch (UnsupportedEncodingException ex) {
@@ -34,6 +43,14 @@ public class ServerResponce implements Serializable {
 
     public byte[] getData() {
         return data;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
     }
     
 }
