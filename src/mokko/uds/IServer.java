5aadb214ae3ff0f18d2613557fcab43291223278
psyriccio <psyriccio@psyzzy.net>
2015-05-29 09:55:00 +0300

    messaging api

--- src/mokko/uds/IServer.java
+++ src/mokko/uds/IServer.java
@@ -14,4 +14,9 @@ public interface IServer {
     public String getHelo();
     public String getVersion();
     
+    public boolean isMessageAvailable(String sessionKey);
+    public Object popMessage(String sessionKey);
+    public Object[] popAllMessages(String sessionKey);
+    public void sendMessage(String sesionKey, String recipient, Object message);
+    
 }
